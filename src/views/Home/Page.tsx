57e3eaa1a4220188e1005875c72638d50bc54951
Milan Shah <milanshah1@gmail.com>
2020-08-02 20:43:31 +0530

    catagory listing restyle

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -83,7 +83,7 @@ const Page: React.FC<{
         </div>
       </div>
       {/* End Hero Section */}
-      <ProductsFeatured />
+
       {categoriesExist() && (
         <div className="home-page__categories">
           <div className="container">
@@ -110,7 +110,24 @@ const Page: React.FC<{
                         })`,
                       }}
                     />
-                    <h3>{category.name}</h3>
+                    <span>
+                      <h3>{category.name}</h3>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="feather feather-arrow-up-right"
+                      >
+                        <line x1="7" y1="17" x2="17" y2="7" />
+                        <polyline points="7 7 17 7 17 17" />
+                      </svg>
+                    </span>
                   </Link>
                 </div>
               ))}
@@ -118,6 +135,7 @@ const Page: React.FC<{
           </div>
         </div>
       )}
+      <ProductsFeatured />
     </>
   );
 };
